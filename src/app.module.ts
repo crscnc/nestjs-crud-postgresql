938960824002769783a025786143d32afcb074df
dc3 <dc3@mailbox.org>
2024-01-05 19:13:16 -0300

    second commit

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CoffeeController } from './coffee/coffee.controller';
+import { CoffeesController } from './coffees/coffees.controller';
+import { CoffeesService } from './coffees/coffees.service';
+import { CoffeesModule } from './coffees/coffees.module';
+
+@Module({
+  imports: [CoffeesModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
